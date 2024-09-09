090ff4a7e1afa06a1980199743f1c0a15d483fa4
Hayley <hayley.mahaney@gmail.com>
2024-09-08 22:28:06 -0400

    coords overlay added

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -1,44 +1,58 @@
-import React, { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useState, useRef } from "react";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
-  const [mapUrl, setMapUrl] = useState("ancien"); // Default map
+  const [mapUrl, setMapUrl] = useState("ancien"); 
+  const [coords, setCoords] = useState({ lat: 0, lng: 0 }); 
 
-  const mapCenter = [-512, 512]; // dont mess with this, u will be big mad
-
+  const mapCenter = [-512, 512]; 
 
   const switchMap = (map) => {
     setMapUrl(map);
   };
+
+  const MapEvents = () => {
+    useMapEvents({
+      mousemove(e) {
+        setCoords(e.latlng);
+      }
+    });
+    return null;
+  };
+
   return (
     <>
-    {/* Overlay for buttons */}
-    <div className="map-overlay">
-      <h3>Select Province</h3>
-      <button onClick={() => switchMap("ancien")}>ancien</button>
-      <button onClick={() => switchMap("innisgallia")}>innisgallia</button>
-      <button onClick={() => switchMap("kerys")}>kerys</button>
-      <button onClick={() => switchMap("merrie")}>merrie</button>
-    </div>
+      {/* Overlay for buttons */}
+      <div className="map-overlay">
+        <h3>Select Province</h3>
+        <button onClick={() => switchMap("ancien")}>ancien</button>
+        <button onClick={() => switchMap("innisgallia")}>innisgallia</button>
+        <button onClick={() => switchMap("kerys")}>kerys</button>
+        <button onClick={() => switchMap("merrie")}>merrie</button>
 
-    <MapContainer
-      center={mapCenter}
-      zoom={0} 
-      minZoom={0}
-      maxZoom={3} 
-      style={{ height: "100vh", width: "100vw" }} 
-      crs={L.CRS.Simple} // Use Simple CRS for non-geographical maps
-      worldCopyJump={false} 
-    >
-      <TileLayer
-        url={`/${mapUrl}/{z}/{x}/{y}.png`}
-        tileSize={512} 
-        noWrap={true} 
-        key={mapUrl}
-      />
-    </MapContainer>
+        <p>Latitude: {-coords.lat.toFixed()}</p>
+        <p>Longitude: {coords.lng.toFixed()}</p>
+      </div>
+
+      <MapContainer
+        center={mapCenter}
+        zoom={0}
+        minZoom={0}
+        maxZoom={3}
+        style={{ height: "100vh", width: "100vw" }}
+        crs={L.CRS.Simple}
+        worldCopyJump={false}
+      >
+        <TileLayer
+          url={`/${mapUrl}/{z}/{x}/{y}.png`}
+          tileSize={512}
+          noWrap={true}
+          key={mapUrl}
+        />
+        <MapEvents />
+      </MapContainer>
     </>
   );
 };
