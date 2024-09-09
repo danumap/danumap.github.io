c6bb86d8941853e570ece6c64521cc836fe98986
Hayley <hayley.mahaney@gmail.com>
2024-09-08 21:20:48 -0400

    initial working test commit

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -4,23 +4,23 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
-  // Define the map's initial center and zoom level
-  const mapCenter = [0, 0]; // Adjust based on your map's starting point
+
+  const mapCenter = [0, 0]; 
 
   return (
     <MapContainer
       center={mapCenter}
-      zoom={0} // Start at zoom level 0
+      zoom={0} 
       minZoom={0}
-      maxZoom={3} // Adjust based on how many zoom levels you have
-      style={{ height: "100vh", width: "100vw" }} // Fullscreen map
+      maxZoom={3} 
+      style={{ height: "100vh", width: "100vw" }} 
       crs={L.CRS.Simple} // Use Simple CRS for non-geographical maps
-      worldCopyJump={false} // Prevent wrapping
+      worldCopyJump={false} 
     >
       <TileLayer
-        url="/ancien/{z}/{x}/{y}.png" // Adjust the path to your tile images
-        tileSize={512} // Set the correct tile size
-        noWrap={true} // Prevent wrapping of the map on edges
+        url="/ancien/{z}/{x}/{y}.png" 
+        tileSize={512} 
+        noWrap={true} 
       />
     </MapContainer>
   );
